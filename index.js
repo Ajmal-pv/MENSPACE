0b3541a0eafac9bbae7adc2b4e7a1403682ef712
MUHAMMAD AJMAL PV <ajmalpv66@gmail.com>
2023-11-29 10:46:29 +0530

    last

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const env = require('dotenv').config()
-
+const mongoose = require("mongoose")
 const express =require("express")
 const app = express();
-// const mongoose = require("mongoose")
-// mongoose.connect("mongodb://127.0.0.1:27017/MENSPACE")
+const session = require('express-session');
+const sessionSecret = "mysessionsecret"
+
 const dbConfig = require('./config/config')
 const user_route = require("./routes/userRoute")
 const admin_route = require("./routes/adminRoute")
@@ -13,8 +14,16 @@ const path = require('path')
 
 const errorHandler = require('./errorHandler')
 
-
-dbConfig.dbConfig()
+mongoose.connect(process.env.mongodb)
+mongoose.set('strictQuery', false)
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to database', err);
+});
+app.use(session({
+    secret:sessionSecret,
+    resave: false,
+    saveUninitialized: true
+  }));
 app.use('/',user_route)
 app.use('/admin',admin_route)
 app.use(express.static(path.join(__dirname, 'public')));
